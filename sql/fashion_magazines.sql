e3d49d01e2174a543f51831077cfa8bbd4863d5f
MouZer77 <skymouser@gmail.com>
2023-10-22 19:55:27 -0400

    Passed the TEST!~!!!!!!!!!!!!!!!!!!

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,8 +1,9 @@
-SELECT orders.*, customers.customer_name, subscriptions.subscription_length, PRINTF(subscriptions.price_per_month * subscriptions.subscription_length) AS Amount Due
+SELECT customers.customer_name AS 'Customer', printf('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS 'Amount Due'
 FROM orders
-JOIN customers
-ON orders.customer_id = customers.customer_id
-JOIN subscriptions
-ON orders.subscription_id = subscriptions.subscription_id
-WHERE order_status = 'unpaid'
-AND subscriptions.description = 'Fashion Magazine';
+JOIN customers 
+    ON orders.customer_id = customers.customer_id
+JOIN subscriptions 
+    ON orders.subscription_id = subscriptions.subscription_id
+WHERE orders.order_status = 'unpaid'
+AND subscriptions.description = 'Fashion Magazine'
+GROUP BY customers.customer_name
